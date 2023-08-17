c236153fc04b10a8cc78370fdf70a49fbb10ad1a
steven-kamanga <kamangasteven00@gmail.com>
2023-08-17 23:08:13 +0200

    changed 1p's

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -61,4 +61,4 @@ def var_num_even_odd(n):
         """
         return render_template("6-number_odd_or_even.html", n=n)
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5004)
